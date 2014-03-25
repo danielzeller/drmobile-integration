a183ae9b0c927ad55e3eb5db5bb0dac46c815d50
Øystein J. Songstad <oystein@aptoma.com>
2014-03-25 14:23:15 +0100

    Change banner script alias attribute

--- js/widget/banner.js
+++ js/widget/banner.js
@@ -9,7 +9,7 @@ define('js/widgets/banner', ['alf'], function(Alf){
             var container = this.container;
             container.$el.empty();
 
-            var url = "http://adserver.adtech.de/addyn|3.0|995.1|3516644|0|1744|ADTECH;cookie=info;alias=iPad_AP_Midtbanner_980x150;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=" + new Date().getTime();
+            var url = "http://adserver.adtech.de/addyn|3.0|995.1|5024934|0|1744|ADTECH;cookie=info;alias=iPad_app_AP_Toppbanner_980x150;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=" + new Date().getTime();
 
             var iframe = document.createElement('iframe');
             iframe.className = 'ad-iframe';
