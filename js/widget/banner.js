10add467b3932d91f6ea17429d9cfb13d4473ee5
Øystein J. Songstad <oystein@aptoma.com>
2014-04-02 15:48:38 +0200

    Add CSS downscaling of oversized phone banners

--- js/widget/banner.js
+++ js/widget/banner.js
@@ -35,14 +35,16 @@ define('js/widgets/phonebanner', ['alf'], function(Alf){
             container.$el.empty();
 
             var url = "http://adserver.adtech.de/addyn/3.0/995/4063904/0/1072/ADTECH;loc=100;target=_blank;grp=145485162;misc=" + new Date().getTime();
-
+            var phoneadwrapper = document.createElement('div');
+            phoneadwrapper.className = 'phone-ad-wrap';
             var iframe = document.createElement('iframe');
             iframe.className = 'ad-iframe';
             iframe.scrolling='no';
             var html = '<body marginwidth="0" marginheight="0" leftmargin="0" topmargin="0" rightmargin="0">'
                 + '<scr'+'ipt language="javascript1.1" src="' + url + '"></scri'+'pt></body>';
             iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
-            container.$el.append(iframe);
+            phoneadwrapper.appendChild(iframe);
+            container.$el.append(phoneadwrapper);
 
             done();
         }
