76a5cd237e045f0afb7dbfd3aa22141ef257f5df
Hein Haraldson Berg <hein@agens.no>
2014-06-11 17:49:53 +0200

    Test removing user-scalable=no for Android Stock Browser

--- js/main.js
+++ js/main.js
@@ -314,7 +314,7 @@ define('main',
                 var windowHeight = $(window).height();
                 var newWidth = windowWidth / windowHeight * 768;
 
-                var $metaViewport = $('<meta name="viewport" content="width=' + newWidth +', user-scalable=no">');
+                var $metaViewport = $('<meta name="viewport" content="width=' + newWidth);// +', user-scalable=no">');
                 $('head').find('meta[name="viewport"]').remove();
                 $('head').append($metaViewport);
             }
