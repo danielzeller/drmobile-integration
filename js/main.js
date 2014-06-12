1d186cd8762230bf242d6e1b05a8af4bcb2cb58b
Hein Haraldson Berg <hein@agens.no>
2014-06-12 13:09:52 +0200

    Log out dimensions for debugging

--- js/main.js
+++ js/main.js
@@ -316,6 +316,10 @@ define('main',
                 var windowHeight = $(window).height();
                 var newWidth = windowWidth / windowHeight * 768;
 
+                this.logToAll('original width: ' + windowWidth);
+                this.logToAll('original height: ' + windowHeight);
+                this.logToAll('new width: ' + newWidth);
+
                 //width=' + newWidth + ', user-scalable=no
                 $('head').find('meta[name="viewport"]').attr('content', 'width=' + newWidth);
             }
