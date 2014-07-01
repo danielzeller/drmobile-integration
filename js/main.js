7e67e50fb557d667764f4c7ffc19616e49bd84e8
Hein Haraldson Berg <hein@agens.no>
2014-07-01 14:04:15 +0200

    Nooope. What’s wrong now..?

--- js/main.js
+++ js/main.js
@@ -338,7 +338,7 @@ define('main',
                 var scaleWidth = $(window).width() / $(window).height() * height;
 
                 //user-scalable=no
-                $('head').find('meta[name="viewport"]').attr('content', 'width=' + scaleWidth);
+                $('head').find('meta[name="viewport"]').attr('content', 'target-densitydpi=device-dpi, width=' + scaleWidth);
             }
         };
 
